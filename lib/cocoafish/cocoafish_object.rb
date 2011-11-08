57373dff8353c0b6cee1198ada01fc551035661e
Wei <wei.kong@cocoafish.com>
2011-11-08 15:10:45 -0800

    fixed json objectify bug with random characters

--- lib/cocoafish/cocoafish_object.rb
+++ lib/cocoafish/cocoafish_object.rb
@@ -9,9 +9,12 @@ module Cocoafish
         end
         v = CocoafishObject.new(v) if v.is_a?(Hash)
         v = v.map {|arrayvalue| arrayvalue.is_a?(Hash) ? CocoafishObject.new(arrayvalue) : arrayvalue} if v.is_a?(Array)
-        self.instance_variable_set("@#{k}", v) ## create and initialize an instance variable for this key/value pair
-        self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")}) ## create the getter that returns the instance variable
-        self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)}) ## create the setter that sets the instance variable
+        begin
+          self.instance_variable_set("@#{k}", v) ## create and initialize an instance variable for this key/value pair
+          self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")}) ## create the getter that returns the instance variable
+          self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)}) ## create the setter that sets the instance variable
+        rescue
+        end
       end
     end
     
